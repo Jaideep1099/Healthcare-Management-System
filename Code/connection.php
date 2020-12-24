6e199495cfaf187794079bf7f5eb611c528f90b0
Jaideep1099 <jaideepkumar1099pm@gmail.com>
2020-12-24 14:49:57 +0530

    Home Template

--- Code/connection.php
+++ Code/connection.php
@@ -1,24 +1,5 @@
 <?php
 
-function dbconnect() {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "HMS";
-
-    try {
-        $conn = new PDO("mysql:host={$servername};dbname={$dbname}", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        return $conn;
-
-    } catch(PDOException $e) {
-        die("Database Connection Failed: ".$e->getMessage());
-    }
-
-}
-
-
 function connect_database(){
     $config = [
         'servername' => 'localhost',
